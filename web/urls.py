73625d792a80300e1ebb7a3a4eb8a061d1c51879
ryuga <vaishnav2353@gmail.com>
2021-06-18 17:59:11 +0530

    Course details page added

--- web/urls.py
+++ web/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.conf.urls import url
-from .views import index_view, EventView
+from .views import index_view, about_view, EventView, CourseView
 
 
 urlpatterns = [
+    url(r'^courses/(?P<slug>.*)', CourseView.as_view(), name="course_details"),
     url(r'^events/(?P<slug>.*)', EventView.as_view(), name="event_details"),
     url(r'^events/', EventView.as_view(), name="events"),
+    url(r'^about/', about_view, name="about"),
     url('', index_view, name="home"),
-    url(r'^about/', index_view, name="about"),
 ]
