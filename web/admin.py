a3dd3d9d3b111c26ac6a4f02bb7eb4ed71dab210
ryuga <vaishnav2353@gmail.com>
2021-06-21 20:50:33 +0530

    Add Alumni and Batch to Admin panel

--- web/admin.py
+++ web/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Event, Faculty, Gallery, Course, Message
+from .models import Event, Faculty, Gallery, Course, Message, Batch, Alumni
 
 admin.site.register(Faculty)
 admin.site.register(Event)
 admin.site.register(Gallery)
 admin.site.register(Course)
 admin.site.register(Message)
+admin.site.register(Batch)
+admin.site.register(Alumni)
