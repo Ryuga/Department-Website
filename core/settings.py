854c664ba2f8d486976cc2ba09057f739263f9d3
ryuga <vaishnav2353@gmail.com>
2021-12-24 14:25:26 +0530

    Update debug

--- core/settings.py
+++ core/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY", default="insecure-secret-key")
 
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 if DEBUG:
     ALLOWED_HOSTS = ['*']
 else:
