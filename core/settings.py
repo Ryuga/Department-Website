9d0fdfee62fbcaabcfc0c3894e73334a00e51a45
ryuga <vaishnav2353@gmail.com>
2021-12-24 18:23:16 +0530

    Update allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 if DEBUG:
     ALLOWED_HOSTS = ['*']
 else:
-    ALLOWED_HOSTS = ["christcs.in", "cs.christci.in"]
+    ALLOWED_HOSTS = ["christcs.in"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -113,6 +113,3 @@ ENCRYPTION_SALT = config("ENCRYPTION_SALT")
 ENCRYPTION_ITERATION = config("ENCRYPTION_ITERATION")
 OAUTH_REDIRECTION_URL = config("OAUTH_REDIRECTION_URL")
 PAYTM_CALLBACK_URL = config("PAYTM_CALLBACK_URL")
-# ssl_certificate         /etc/ssl/certs/cert.pem;
-# ssl_certificate_key     /etc/ssl/private/key.pem;
-# ssl_client_certificate /etc/ssl/certs/cloudflare.crt;
