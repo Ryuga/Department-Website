9e8867e0118932e3dbfae4afee92c42c49d6bc0c
Vaishnav <47474919+Ryuga@users.noreply.github.com>
2022-04-07 23:17:58 +0530

    Hide Abstract models

--- core/apps/dashboard/admin.py
+++ core/apps/dashboard/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import (
-    Student, Program, Slideshow, Registration, Transaction,
-    Event, EventDay, EventSchedule, TransactionProgramRelation
+    Student, Program, Slideshow, Transaction,
+    Event, EventDay, EventSchedule, Registration,
 )
 
 admin.site.register([
-    Student, Program, Slideshow, Registration, Transaction,
-    Event, EventDay, EventSchedule, TransactionProgramRelation
+    Student, Program, Slideshow, Transaction,
+    Event, EventDay, EventSchedule, Registration,
 ])
