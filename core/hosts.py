dcf321cf47d238361cd75b808feb8cb6da47878d
Ryuga <vaishnav2353@gmail.com>
2023-10-14 18:46:06 +0530

    Event registration button fix

--- core/hosts.py
+++ core/hosts.py
@@ -3,6 +3,7 @@ from django_hosts import patterns, host
 host_patterns = patterns(
     '',
     host(r'www', 'core.apps.web.urls', name='www'),
-    host(r'zephyrus', 'core.apps.dashboard.urls', name='dashboard'),
+    host(r'zephyrus', 'core.apps.dashboard.urls', name='zephyrus'),
+    host(r'dashboard', 'core.apps.dashboard.urls', name='dashboard'),
     host(r'admin', 'core.urls', name='admin'),
 )
