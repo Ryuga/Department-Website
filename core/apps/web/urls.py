64f9096cc1c680b266b5fba1f600a72fc861dd3f
Ryuga <vaishnav2353@gmail.com>
2025-01-10 13:04:03 +0530

    Minor update

--- core/apps/web/urls.py
+++ core/apps/web/urls.py
@@ -1,4 +1,4 @@
-from django.urls import re_path
+f.rom django.urls import re_path
 from .views import (
     IndexView, AboutView, lazy_load_faculty, GalleryListView,
     EventView, CourseView, AlumniListView, dashboard_redirect,
